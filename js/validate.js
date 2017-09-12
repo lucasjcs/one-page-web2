189c641ddc85d4ae12c0e8b8e593831787fa13d7
Lucas Jacinto da Silva <silval@alunos.utfpr.edu.br>
2017-09-12 16:30:07 -0300

    Enviando arquivos, primeira versão

---
+++ js/validate.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    var $doc = $('html, body');
+    $('a').click(function() {
+        $doc.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+});
